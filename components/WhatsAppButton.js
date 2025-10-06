8f089bda9347edf4d2f975e8b92818de50f78415
Akshay Zingade <akshayzingade@Akshays-MacBook-Air.local>
2025-10-06 22:07:12 +0530

    Update WhatsApp number

--- components/WhatsAppButton.js
+++ components/WhatsAppButton.js
@@ -7,7 +7,7 @@ import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/919561845858?text=Hi%2C%20I%20am%20interested%20in%20custom%20hospital%20printing.%20Please%20assist."
+      href="https://wa.me/918050260636?text=Hi%2C%20I%20am%20interested%20in%20custom%20hospital%20printing.%20Please%20assist."
       className={styles.whatsapp}
       target="_blank"
       rel="noopener noreferrer"
